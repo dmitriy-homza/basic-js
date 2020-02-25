b37532f6fc57accc55c85758aad0f498418a6944
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-02-25 18:15:48 +0300

    fix: count-cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix){
+    let number = 0;
+    for (i=0; i<matrix.length; i++) {
+        for (z=0; z<matrix[i].length; z++) {
+          console.log('Элемент: '+matrix[i][z]);
+if (matrix[i][z]==='^^') {
+              number = number+1;
+          }
+        }};
+  return number;
+}
