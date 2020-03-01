a9b671be0984021f0984203e4a7da82d158dfae2
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-01 22:06:43 +0300

    feat: dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let answer = '';
+if (members!=undefined & members!=null & typeof(members)==="object" || typeof(members)==="string") {
+  if (members[0]) {
+  for (let name of members) {
+    if (typeof(name)==="string"){
+    let regExp = name.match(/[A-Z]/i);
+    answer = answer+regExp[0];}
+  }
+return(answer.toUpperCase().split('').sort().join(''));}
+else {
+  return(false);
+}
+}
+else {
+  return(false);
+}
 };
