4d546a7f15b10599979af0e8bb68b36438c3ec54
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-01 21:10:41 +0300

    feat: carbon dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(input) {
+if (typeof(input)==="string" & input>0 & input<15) {
+    let age = Math.ceil(Math.log(15/input)/(0.693/5730));
+    return (age);
+  }
+  else if (typeof(input)==="string" & input*0!=0) {
+      input = input.slice(0, input.indexOf('.', 3))+input.slice((input.indexOf('.',3)+1), input.length);
+      if (typeof(input)==="string" & input>0 & input<15) {
+        let age = Math.ceil(Math.log(15/input)/(0.693/5730));
+        return (age);}
+        else {
+          return false;
+        }
+      }
+else {
+      return false;
+    }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,6 @@ module.exports = function countCats(matrix){
     let number = 0;
     for (i=0; i<matrix.length; i++) {
         for (z=0; z<matrix[i].length; z++) {
-          console.log('Элемент: '+matrix[i][z]);
 if (matrix[i][z]==='^^') {
               number = number+1;
           }
