2687162e0e3db024d1576a3d7995ab3ae15b5585
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-03 01:56:33 +0300

    feat: what-season done

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(date) {
+  try {
+      if (date === getMonth){
+      throw('Not implemented');
+    };
+    let monthNumber = date.getMonth();
+    console.log(monthNumber);
+      if (monthNumber < 2 || monthNumber > 10) {
+      return('winter')
+    }
+    else if (monthNumber > 1 & monthNumber < 5 ) {
+      return('spring')
+    }
+    else if (monthNumber > 4 & monthNumber < 8) {
+      return('summer')
+    }
+    else if (monthNumber > 7 & monthNumber < 11) {
+      return('autumn')
+    }
+  }
+
+  catch (error) {
+    throw 'Not implemented';
+  }
   // remove line with error and write your code here
 };
