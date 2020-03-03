054a280a519a7045bb2e8bb5f887f7f2edc31187
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-03 03:00:10 +0300

    feat: what-season already done

--- src/what-season.js
+++ src/what-season.js
@@ -1,10 +1,10 @@
 module.exports = function getSeason(date) {
   try {
-      if (date === getMonth){
-      throw('Not implemented');
-    };
+    if (date.hasOwnProperty('getMonth')) {
+      throw new Error;
+    }
+    else if (arguments.length != 0) {
     let monthNumber = date.getMonth();
-    console.log(monthNumber);
       if (monthNumber < 2 || monthNumber > 10) {
       return('winter')
     }
@@ -17,10 +17,15 @@ module.exports = function getSeason(date) {
     else if (monthNumber > 7 & monthNumber < 11) {
       return('autumn')
     }
-  }
 
+    }
+
+}
   catch (error) {
-    throw 'Not implemented';
+ if (arguments.length == 0) {
+      return ('Unable to determine the time of year!')
+    }
+else {
+    throw new Error()};
   }
-  // remove line with error and write your code here
 };
