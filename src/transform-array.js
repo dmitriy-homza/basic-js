3e983ca9797393ca6dabe260fd661d65d45911c4
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-09 02:01:00 +0300

    feat: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,85 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(array) {
+
+for (let z=0; z<array.length; z++) {
+  array.forEach(function(item, i) {
+    if (array[i]=='undefined') {
+      array.splice((i), 1)
+    }
+    if (array.length!=1) {
+      if (item === '--double-prev')
+          {array[i]=array[i-1];}
+}});};
+
+for (let z=0; z<array.length; z++) {
+      array.forEach(function(item, i) {
+      if (array.length!=1) {
+        if (item === '--double-next')
+            {array[i]=array[i+1];}
+}});};
+
+for (let z=0; z<array.length; z++) {
+      array.forEach(function(item, i) {
+              if (array.length!=1) {
+                if (item === '--discard-next')
+                    {array.splice(i, 2);}
+}});};
+
+for (let z=0; z<array.length; z++) {
+      array.forEach(function(item, i) {
+        if (array.length!=1 & i!=0) {
+          if (item === '--discard-prev')
+              {array.splice((i-1), 2);}
+}});};
+
+
+array.forEach(function(item, i) {
+            if (array[i]==undefined || array[i]=='--discard-next' || array[i]=='--discard-prev' || array[i]=='--double-next' || array[i]=='--double-prev') {
+              array.splice((i), 1)
+}});
+  return(array);
 };
+
+
+/* function transform(array) {
+    console.log('Входной массив: '+array);
+  array.forEach(function(item, i) {
+    if (array.length!=1) {
+    if (array[0]==='--discard-prev' || array[0]==='--double-prev'){
+      array.splice(i, 1);
+    }
+         if (i!=(array.length-1)) {
+console.log('берем элемент '+ array[i]);
+            if (item === '--double-next' & (array[i+1]))
+                {array[i]=array[i+1]; i=i-1;}
+            else if (item === '--double-next') {
+                array.splice(i, 1); i=i-1;}
+            else if (item === '--discard-prev' & (array[i-1]))
+                {array.splice((i-1), 2); i=i-1;}
+            else if (item === '--discard-prev')
+                {array.splice((i), 1); i=i-1;}
+            else if (item === '--discard-next')
+                {array.splice(i, 2); i=i-1;}
+            else if (item === '--double-prev' & (array[i-1]))
+                {array[i]=array[i-1]; i=i-1;}
+            else if (item === '--double-prev') {
+                array.splice(i, 1); i=i-1;}}
+        else { console.log('Последний элемент '+ array[i] + ' из массива ' + array);
+            if (item === '--double-next') {
+                array.splice(i, 1);}
+            else if (item === '--discard-prev' & (array[i-1]))
+                {array.splice((i-1), 2);}
+            else if (item === '--discard-prev')
+                {array.splice((i), 1);}
+            else if (item === '--discard-next')
+                {array.splice(i, 1);}
+            else if (item === '--double-prev')
+                {array[i]=array[i-1];}}
+  }
+  else if (typeof(array[0]!='string') || typeof(array[0]!='number') || array[0]=='--discard-prev' || array[0]=='--double-prev' || array[0]=='--discard-next' || array[0]=='--double-next'){array=[]};
+
+}
+);
+if (array[0]=='--discard-prev' || array[0]=='--double-prev' || array[0]=='--discard-next' || array[0]=='--double-next'){array=[]};
+      console.log('Выходной массив: '+array);
+  return(array);
+};*/
