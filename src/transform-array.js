6113f7685bd094302199edece8733bd28bc23995
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-10-03 23:06:04 +0300

    fix: some transform array errors

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(array) {
+module.exports = function transform(arrayInput) {
+  array = arrayInput.concat();
   for (let z = 0; z < array.length; z++) {
     array.forEach(function (item, i) {
+      if (array[i] === "--discard-next" && array[i + 2] === "--discard-prev") {
+        array.splice(i, 3);
+      }
+      if (array[i] === "--discard-next" && array[i + 2] === "--double-prev") {
+        array.splice(i, 3);
+      }
       if (array[i] == "undefined") {
         array.splice(i, 1);
       }
@@ -57,46 +64,3 @@ module.exports = function transform(array) {
   });
   return array;
 };
-/* function transform(array) {
-    console.log('Входной массив: '+array);
-  array.forEach(function(item, i) {
-    if (array.length!=1) {
-    if (array[0]==='--discard-prev' || array[0]==='--double-prev'){
-      array.splice(i, 1);
-    }
-         if (i!=(array.length-1)) {
-console.log('берем элемент '+ array[i]);
-            if (item === '--double-next' & (array[i+1]))
-                {array[i]=array[i+1]; i=i-1;}
-            else if (item === '--double-next') {
-                array.splice(i, 1); i=i-1;}
-            else if (item === '--discard-prev' & (array[i-1]))
-                {array.splice((i-1), 2); i=i-1;}
-            else if (item === '--discard-prev')
-                {array.splice((i), 1); i=i-1;}
-            else if (item === '--discard-next')
-                {array.splice(i, 2); i=i-1;}
-            else if (item === '--double-prev' & (array[i-1]))
-                {array[i]=array[i-1]; i=i-1;}
-            else if (item === '--double-prev') {
-                array.splice(i, 1); i=i-1;}}
-        else { console.log('Последний элемент '+ array[i] + ' из массива ' + array);
-            if (item === '--double-next') {
-                array.splice(i, 1);}
-            else if (item === '--discard-prev' & (array[i-1]))
-                {array.splice((i-1), 2);}
-            else if (item === '--discard-prev')
-                {array.splice((i), 1);}
-            else if (item === '--discard-next')
-                {array.splice(i, 1);}
-            else if (item === '--double-prev')
-                {array[i]=array[i-1];}}
-  }
-  else if (typeof(array[0]!='string') || typeof(array[0]!='number') || array[0]=='--discard-prev' || array[0]=='--double-prev' || array[0]=='--discard-next' || array[0]=='--double-next'){array=[]};
-
-}
-);
-if (array[0]=='--discard-prev' || array[0]=='--double-prev' || array[0]=='--discard-next' || array[0]=='--double-next'){array=[]};
-      console.log('Выходной массив: '+array);
-  return(array);
-};*/
